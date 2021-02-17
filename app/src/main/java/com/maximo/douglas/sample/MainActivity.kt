9c5f50dbb17569de33b8c3a402b81c176cdc38e1
MaximoDouglas <douglas.h.maximo@gmail.com>
2021-02-17 15:16:53 -0300

    New package name test

--- app/src/main/java/com/maximo/douglas/sample/MainActivity.kt
+++ app/src/main/java/com/maximo/douglas/sample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.maximo.douglas.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.maximo.douglas.mylibrary.utils.sum
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sum(1, 2)
     }
 }
